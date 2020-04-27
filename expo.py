91a68f864d494b578ebfc771d433fb69b6858f43
Carl <carlbechie@carlbechie.com>
2020-04-27 12:01:50 -0700

    First push

---
+++ expo.py
@@ -0,0 +1,10 @@
+def expo(base, exponent):
+    # Write your function here
+
+def main():
+    """Tests with powers of 2."""
+    for exponent in range (5):
+        print(exponent, expo(2, exponent))
+
+if __name__ == "__main__":
+    main()
